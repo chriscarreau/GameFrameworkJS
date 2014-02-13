eab4b21ce3ecb4c157382a55fd1568d365ac2c6f
Kech <stephane_kech@hotmail.com>
2014-02-12 21:33:34 -0500

    Game Start

--- Game.js
+++ Game.js
@@ -2,3 +2,21 @@ var canvas;
 window.onload = function(){
 	canvas = document.getElementById
 }
+
+var index = 20;
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+(function animloop(){
+  requestAnimFrame(animloop);
+  render();
+})();
+
+ctx.rect(index,20,150,100);
+ctx.stroke();
