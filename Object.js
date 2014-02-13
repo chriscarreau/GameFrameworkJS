289173fc242fa1296b74a12e1d7da8673dd4bd50
Kech <stephane_kech@hotmail.com>
2014-02-13 17:49:20 -0500

    Template for object.js

--- Object.js
+++ Object.js
@@ -1,10 +1,44 @@
-var height = 600, //si non spécifé, prendre celle de l'image/sprite
-	width = 1000,
-	accelX = 0,
-	accelY = 0,
-	posX = 0,
-	posY = 0;
-	dir = 'Left';
-	sprite = 'Sprite';
-	gravity = 0;
-	isAffectedByGravity = true;
+var GameObject = function(options)
+{
+	//----------------------------------------------------
+	// GLOBAL VARIABLES
+	//----------------------------------------------------
+	var height = 600, //si non spécifé, prendre celle de l'image/sprite
+		width = 1000,
+		accelX = 0,
+		accelY = 0,
+		posX = 0,
+		posY = 0;
+		dir = 'Left';
+		sprite = 'Sprite';
+		gravity = 0;
+		isAffectedByGravity = true;
+		
+	//----------------------------------------------------
+	// RENDER
+	//----------------------------------------------------
+	function render{
+		//Code pour render les objets
+	}
+
+	//----------------------------------------------------
+	// UPDATE
+	//----------------------------------------------------
+	function update{
+		//Code pour updater les objets;
+	}
+
+	//----------------------------------------------------
+	// CHECK COLLISION
+	//----------------------------------------------------
+	function checkCollision{
+		//Code pou vérifier la collision
+	}
+
+	//----------------------------------------------------
+	// ON COLLISION
+	//----------------------------------------------------
+	function onCollision{
+		//To be defined by the coder
+	}
+}
