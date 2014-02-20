09faa7fd802c88ad8f39f69e0a35f260a76d1ad1
Christophe Carreau-Lacasse <chris.carreau@hotmail.com>
2014-02-19 20:05:08 -0500

    yolo

--- Map.js
+++ Map.js
@@ -5,12 +5,12 @@ var TiledMap = function(options)
 		Map.mapUrl = options.src,						//url of the map on the server 
 		Map.Name = options.name, 						//name you want to give to the map
 		Map.JsonObject = loadMap(),						//Json object containing all the information of the map
-		Map.Height = JsonObject.height,					//Height of the map
-		Map.Width = JsonObject.width,					//Width of the map
-		Map.TileWidth = JsonObject.tilewidth,			//width of a tile
-		Map.TileHeight = JsonObject.tileheight,			//height of a tile
-		Map.Layers = JsonObject.layers,					//collection of all the layers
-		Map.TileSets = JsonObject.tilesets,				//collection of tilesets
+		Map.Height = Map.JsonObject.height,					//Height of the map
+		Map.Width = Map.JsonObject.width,					//Width of the map
+		Map.TileWidth = Map.JsonObject.tilewidth,			//width of a tile
+		Map.TileHeight = Map.JsonObject.tileheight,			//height of a tile
+		Map.Layers = Map.JsonObject.layers,					//collection of all the layers
+		Map.TileSets = Map.JsonObject.tilesets,				//collection of tilesets
 		Map.IsScrolling = options.isScrolling || false,	//Tell if the background should scroll
 		Map.PositionX = options.positionX || 0,
 		Map.PositionY = options.positionY || 0,
@@ -22,7 +22,7 @@ var TiledMap = function(options)
 	{
 		if (Map.IsScrolling)
 		{
-			if(Map.ScrollDirection = 'Horizontal')
+			if(Map.ScrollDirection == 'Horizontal')
 			{
 				Map.PositionX += Map.ScrollSpeed
 			}
